f412cff3f95a71e2ac557721d4db94a31c55084d
Adeyemi Babatope Silas <babatopesilasadeyemi@gmail.com>
2026-04-19 22:04:06 +0100

    Create q3.js

---
+++ q3.js
@@ -0,0 +1,9 @@
+// Q3: Greet User
+
+// Function to greet a user
+function greetUser(name) {
+  return `Welcome to TechCrush, ${name}!`;
+}
+
+// Testing the function
+console.log(greetUser("Adeyemi Silas"));
