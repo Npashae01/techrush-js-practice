b8a0778094f1c7b71f9dc7010800946147814665
Adeyemi Babatope Silas <babatopesilasadeyemi@gmail.com>
2026-04-19 22:14:52 +0100

    Create q5.js

---
+++ q5.js
@@ -0,0 +1,8 @@
+// Q5: using map()
+
+const numbers = [3, 6, 9, 12, 15];
+
+// double each number and store in a new array
+const doubledNumbers = numbers.map(num => num * 2);
+
+console.log(doubledNumbers);
