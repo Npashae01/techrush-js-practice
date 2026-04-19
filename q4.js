8f23a0123070d5d220ea59b7912c37c8aed04de2
Adeyemi Babatope Silas <babatopesilasadeyemi@gmail.com>
2026-04-19 22:10:07 +0100

    Create q4.js

---
+++ q4.js
@@ -0,0 +1,13 @@
+// Q4: Arrays
+
+const fruits = ["mango", "orange", "banana"];
+
+// Add 'pineapple' to the end
+fruits.push("pineapple");
+
+// Remove the first item
+fruits.shift();
+
+// Log updated array and its length
+console.log(fruits);
+console.log(fruits.length);
